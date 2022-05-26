7c604f59835f79a6d89ccecf2ba44ea868c49b1a
ani9977 <aniketsirota64@gmail.com>
2022-05-25 19:18:04 -0700

    message

---
+++ Exception/TestTryCatch.java
@@ -0,0 +1,23 @@
+package Exception;
+
+public class TestTryCatch {
+	
+	public static void main(String[] args) {
+		
+		int a = 0;
+		int b = 18;
+		int c = 15;
+		int d = 20;
+        try {		
+	    double div = b/a*d-c;
+        }       catch(ArithmeticException e) {
+        	System.out.println("wrong method");
+        }
+        }}
+
+		
+	
+	
+	
+
+
